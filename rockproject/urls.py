37a9059729b5f83051cd085da885eaccd35cc487
Jean Patimeteeporn <jpatimet@gmail.com>
2024-03-19 20:27:19 -0500

    set up a view and model for rocks and types

--- rockproject/urls.py
+++ rockproject/urls.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from rockapi.views import register_user, login_user
+from django.conf.urls import include
+from rest_framework import routers
+from rockapi.views import TypeView
 
 router = routers.DefaultRouter(trailing_slash=False)
 
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r"types", TypeView, "type")
+
 urlpatterns = [
-    path('', include(router.urls)),
-    path('register', register_user),
-    path('login', login_user),
-    path('admin/', admin.site.urls),
+    path("", include(router.urls)),
+    path("register", register_user),  # Enables http://localhost:8000/register
+    path("login", login_user),  # Enables http://localhost:8000/login
+    path("admin/", admin.site.urls),
 ]
